ff60cb3530cbc7b7229035f2fa593599774ab8d7
lokesh007-crypto <lokesh86d@gmail.com>
2026-09-22 22:14:19 +0530

    Update proj_1.py

--- proj_1.py
+++ proj_1.py
@@ -1,9 +1,6 @@
-import streamlit as st 
 import pandas as pd
 import requests
 from datetime import datetime
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 ##deriving data
 Earthquake_records =[]
